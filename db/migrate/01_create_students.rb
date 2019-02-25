4be5bbe5174617dfef38f567cd5379c392061d0b
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-02-25 20:16:01 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,15 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+<<<<<<< HEAD
     def change
         create_table :students do |s|
             s.string :name
         end
+=======
+
+    def change
+        create_table :students do |s|
+            s.string :name 
+        
+>>>>>>> 5c5785e4e9641bb458cf9886789d60b8da8ca0b2
     end
 end
